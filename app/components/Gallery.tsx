fd3ef565e81944a490c1bd84e386203d8dbb249d
MaheshiNav <maheshinawarathna29@gmail.com>
2024-06-23 18:08:49 -0700

    add picture to the gallery of portfolio

--- app/components/Gallery.tsx
+++ app/components/Gallery.tsx
@@ -69,9 +69,12 @@ const ProjectsGallery: React.FC<ProjectsGalleryProps> = ({ activeTab }) => {
       images: [
         "https://res.cloudinary.com/maheshidevelopments/image/upload/c_fill,w_769,h_433,ar_16:9/v1719189200/wonderwriters-ss-02.png",
         "https://res.cloudinary.com/maheshidevelopments/image/upload/c_fill,w_769,h_433,ar_16:9/v1719189612/wonderwriters-ss-03.png",
+        "https://res.cloudinary.com/maheshidevelopments/image/upload/c_crop,w_769,h_433,ar_16:9/v1719191176/ww-ss-07.png",
         "https://res.cloudinary.com/maheshidevelopments/image/upload/c_crop,w_769,h_433,ar_16:9/v1719190202/ww-ss-04.png",
         "https://res.cloudinary.com/maheshidevelopments/image/upload/b_rgb:C2C9D6/c_fill,w_769,h_433,ar_16:9/v1719190305/wonderwriters-04.png",
-        "https://res.cloudinary.com/maheshidevelopments/image/upload/c_fill,w_769,h_433,ar_16:9/v1719190534/wonderwriters-03.png"
+        "https://res.cloudinary.com/maheshidevelopments/image/upload/c_fill,w_769,h_433,ar_16:9/v1719190534/wonderwriters-03.png",
+        "https://res.cloudinary.com/maheshidevelopments/image/upload/c_fill,w_769,h_433,ar_16:9/v1719191188/ww-ss-08.png",
+
       ],
     },
 

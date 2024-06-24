b118c96ffdf8b5bb2a84e594d4fcd43e6ee0c9fc
Maheshi Nawarathna <maheshinawarathna32@gmail.com>
2024-06-24 00:36:47 -0700

    Update Gallery.tsx with new description for WonderWriters

--- app/components/Gallery.tsx
+++ app/components/Gallery.tsx
@@ -53,7 +53,7 @@ const ProjectsGallery: React.FC<ProjectsGalleryProps> = ({ activeTab }) => {
       projectName: "WonderWriters",
       subtitle: "Drawing, illustrating and sharing stories",
       description:
-        "An application for story writers and illustrators to collaborate with each and make beautiful stories for kids.",
+        "An application for story writers and illustrators to collaborate with each and make beautiful children stories. My hobby of writing/illustrating drove me to do this project.All the cartoons and Website design is my own",
 
       technologies: [
         "Typescript",
